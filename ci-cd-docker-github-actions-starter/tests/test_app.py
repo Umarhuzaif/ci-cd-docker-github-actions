c1db470d5374c094872510b95c531e659a42bcf9
Umar Huzaif Kolhar <vumarhuzaif@gmail.com>
2025-08-20 15:05:20 +0530

    Fix lint issues with Ruff

--- ci-cd-docker-github-actions-starter/tests/test_app.py
+++ ci-cd-docker-github-actions-starter/tests/test_app.py
@@ -1,10 +1,12 @@
 from app.app import app
 
+
 def test_health():
     client = app.test_client()
     resp = client.get("/health")
     assert resp.status_code == 200
     assert resp.get_json().get("status") == "ok"
+
 
 def test_root():
     client = app.test_client()
